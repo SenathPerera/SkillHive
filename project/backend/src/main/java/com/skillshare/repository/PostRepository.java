a039103f3c2cdbba1810c75984ad1e9befa70d2e
PraveenDevinda20 <devpraveen20@gmail.com>
2025-05-01 08:38:10 +0530

    PostRepository.java added

---
+++ project/backend/src/main/java/com/skillshare/repository/PostRepository.java
@@ -0,0 +1,8 @@
+import com.skillshare.model.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.List;
+
+public interface PostRepository extends MongoRepository<Post, String> {
+    List<Post> findByUserIdOrderByCreatedAtDesc(String userId);
+    List<Post> findAllByOrderByCreatedAtDesc();
+}
