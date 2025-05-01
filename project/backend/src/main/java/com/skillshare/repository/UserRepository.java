99eb79b0e0a316192e44a71e2d166f6f5cfae5b6
ASUS <sarindusamarasekara@gmail.com>
2025-05-02 01:30:29 +0530

    commit-11/UserRepository.java

--- project/backend/src/main/java/com/skillshare/repository/UserRepository.java
+++ project/backend/src/main/java/com/skillshare/repository/UserRepository.java
@@ -5,3 +5,9 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
 import java.util.Optional;
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+
+    List<User> findAllByIdIn(List<String> ids);
+}
