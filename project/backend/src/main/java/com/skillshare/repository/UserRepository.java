aa46262a9308109ce4899abc9754e7c2fd302471
ASUS <sarindusamarasekara@gmail.com>
2025-05-02 00:29:07 +0530

    commit-07/UserController.java

---
+++ project/backend/src/main/java/com/skillshare/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.skillshare.repository;
+
+import com.skillshare.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+import java.util.Optional;
